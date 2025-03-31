6027cf12af777af22fc34c3339c1c87bd40aa06e
Rijarilala <rijarilalarasolofomanana@gmail.com>
2025-03-31 10:26:52 +0300

    handrana

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -238,11 +238,7 @@ const Index = () => {
             ))}
           </div>
 
-          <div className="text-center mt-12">
-            <Button asChild variant="outline">
-              <Link to="/#">Voir plus de témoignages</Link>
-            </Button>
-          </div>
+          
         </div>
       </section>
 
